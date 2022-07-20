0b175600af13d3541cc1ed31d1989f342f47e041
AndresFelipe2002 <aperezurriago5@gmail.com>
2022-07-20 10:28:16 -0500

    proyecto

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiNet.Context
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+
+        {
+                
+        }
+        public DbSet<Gestores_Bd> gestores_bd { get; set; }
+    }
+}
